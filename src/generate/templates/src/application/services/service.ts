2d577134c6f99053b152c60158a98274e720547c
cfaboumyIppon <114093660+cfaboumyIppon@users.noreply.github.com>
2023-02-21 10:25:41 +0100

    feat : refactor service template to handle manyOnly

--- src/generate/templates/src/application/services/service.ts
+++ src/generate/templates/src/application/services/service.ts
@@ -10,19 +10,19 @@ export function createService(
   _tree: Tree,
   projectName: string
 ) {
-    let [paginationArgsImport, fieldPaginationImport, fieldPaginationMethod] = computePaginationTemplates(types, type);
-    let [
-      forwardRefAndInjectImport,
-      forwardReferencedServices,
-      referencedServices,
-      modelsAndServices,
-      createRelationships,
-      initRelationships,
-      updateRelationships,
-      relatedRepositoryImport
-      ] = computeRelationshipsTemplates(type);
-    let updateFields = computeFieldTemplate(type)
-    let fileTemplate = `${handleIdGeneration(type)[0]}import { ${forwardRefAndInjectImport}Injectable } from '@nestjs/common';
+  let [paginationArgsImport, fieldPaginationImport, fieldPaginationMethod] = computePaginationTemplates(types, type);
+  let [
+    forwardRefAndInjectImport,
+    forwardReferencedServices,
+    referencedServices,
+    modelsAndServices,
+    createRelationships,
+    initRelationships,
+    updateRelationships,
+    relatedRepositoryImport
+  ] = computeRelationshipsTemplates(types, type);
+  let updateFields = computeFieldTemplate(type)
+  let fileTemplate = `${handleIdGeneration(type)[0]}import { ${forwardRefAndInjectImport}Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { ${type.typeName} } from 'adapters/typeorm/entities/${strings.camelize(type.typeName)}.model';
@@ -101,28 +101,28 @@ export class ${type.typeName}Service implements I${type.typeName}Service {
 }
 `;
 
-    _tree.create(
-      `${projectName}/src/application/services/${strings.camelize(
-        type.typeName
-      )}.service.ts`,
-      fileTemplate
-    );
+  _tree.create(
+    `${projectName}/src/application/services/${strings.camelize(
+      type.typeName
+    )}.service.ts`,
+    fileTemplate
+  );
 }
 
 function computeFieldTemplate(type: Type): string {
   let updateFields = '';
-  const scalarAndEnumFields = type.fields.filter((field: Field) => field.type !== 'ID' && !field.isDeprecated &&(!field.relation || field.isEnum));
+  const scalarAndEnumFields = type.fields.filter((field: Field) => field.type !== 'ID' && !field.isDeprecated && (!field.relation || field.isEnum));
   if (scalarAndEnumFields.length > 0) {
     scalarAndEnumFields.forEach((field) => {
-        updateFields += `
+      updateFields += `
     ${strings.camelize(type.typeName)}.${field.name} = input.${strings.camelize(field.name)};`;
-      });
+    });
   }
   return updateFields;
 }
 
-function computeRelationshipsTemplates(type: Type): string[] {
-let [forwardRefAndInjectImport, forwardReferencedServices, referencedServices] = computeForwardRelationships(type);
+function computeRelationshipsTemplates(types: Type[], type: Type): string[] {
+  let [forwardRefAndInjectImport, forwardReferencedServices, referencedServices] = computeForwardRelationships(types, type);
   let modelsAndServices = '';
   let createRelationships = '';
   let initRelationships = '';
@@ -138,10 +138,10 @@ let [forwardRefAndInjectImport, forwardReferencedServices, referencedServices] =
 import { ${relationship.type} } from 'adapters/typeorm/entities/${strings.camelize(relationship.type)}.model';
 import { ${relationship.type}Service } from './${strings.camelize(relationship.type)}.service';`;
       }
-        if (relationship.isArray) {
-          createRelationships += `\n    const mock${relationship.type} = new ${relationship.type}();`;
-          if (relationship.type === type.typeName) {
-            createRelationships += `
+      if (relationship.isArray) {
+        createRelationships += `\n    const mock${relationship.type} = new ${relationship.type}();`;
+        if (relationship.type === type.typeName) {
+          createRelationships += `
     ${strings.camelize(type.typeName)}.child${strings.capitalize(pluralize(relationship.name))} = [mock${relationship.type}];
     if (input.${strings.camelize(pluralize(relationship.name, 1))}Ids && input.${strings.camelize(pluralize(relationship.name, 1))}Ids.length > 0) {
       for (let i = 0; i < input.${strings.camelize(pluralize(relationship.name, 1))}Ids.length; i++) {
@@ -153,7 +153,7 @@ import { ${relationship.type}Service } from './${strings.camelize(relationship.t
         }
       }
     }`;
-          updateRelationships +=`
+          updateRelationships += `
     if (input.${strings.camelize(pluralize(relationship.name, 1))}Ids) {
       const linked${strings.capitalize(relationship.name)}: ${relationship.type}[] = [];
       input.${strings.camelize(pluralize(relationship.name, 1))}Ids.forEach(async (${strings.camelize(pluralize(relationship.name, 1))}Id) => {
@@ -167,8 +167,8 @@ import { ${relationship.type}Service } from './${strings.camelize(relationship.t
       });
       ${strings.camelize(type.typeName)}.child${strings.capitalize(pluralize(relationship.name))} = linked${strings.capitalize(relationship.name)};
     }`;
-          } else {
-            createRelationships += `
+        } else {
+          createRelationships += `
     ${strings.camelize(type.typeName)}.${strings.camelize(relationship.name)} = [mock${relationship.type}];
     if (input.${strings.camelize(pluralize(relationship.name, 1))}Ids && input.${strings.camelize(pluralize(relationship.name, 1))}Ids.length > 0) {
       for (let i = 0; i < input.${strings.camelize(pluralize(relationship.name, 1))}Ids.length; i++) {
@@ -178,7 +178,7 @@ import { ${relationship.type}Service } from './${strings.camelize(relationship.t
         ${strings.camelize(type.typeName)}.${strings.camelize(relationship.name)}[i].id = ${strings.camelize(pluralize(relationship.name, 1))}.id;
       }
     }`;
-          updateRelationships +=`
+          updateRelationships += `
     if (input.${strings.camelize(pluralize(relationship.name, 1))}Ids) {
       const linked${strings.capitalize(relationship.name)}: ${relationship.type}[] = [];
       input.${strings.camelize(pluralize(relationship.name, 1))}Ids.forEach(async (${strings.camelize(pluralize(relationship.name, 1))}Id) => {
@@ -191,14 +191,14 @@ import { ${relationship.type}Service } from './${strings.camelize(relationship.t
           relatedRepositoryImport += `
     @InjectRepository(${relationship.type})
     private readonly ${strings.camelize(relationship.type)}Repository: Repository<${relationship.type}>,`;
-          
-          }
-          
-        } else {
-          createRelationships += `
+
+        }
+
+      } else {
+        createRelationships += `
     ${strings.camelize(type.typeName)}.${relationship.name} = new ${relationship.type}();`;
-          if (relationship.type === type.typeName) {
-            initRelationships += `
+        if (relationship.type === type.typeName) {
+          initRelationships += `
     if (input.${relationship.name}Id && input.${relationship.name}Id !== ${strings.camelize(type.typeName)}.id) {
       const ${relationship.name} = await this.${strings.camelize(relationship.type)}Repository.findOneOrFail({
         where: { id: input.${relationship.name}Id },
@@ -213,7 +213,7 @@ import { ${relationship.type}Service } from './${strings.camelize(relationship.t
       });
       ${strings.camelize(type.typeName)}.${relationship.name}.id = ${relationship.name}.id;
     }`;
-          } else {
+        } else {
           initRelationships += `
     if (input.${relationship.name}Id) {
       const ${relationship.name} = await this.${strings.camelize(relationship.type)}Service.${strings.camelize(relationship.type)}GetById(input.${relationship.name}Id);
@@ -229,6 +229,12 @@ import { ${relationship.type}Service } from './${strings.camelize(relationship.t
       }
     });
   }
+  if (computeManyOnlyRelationships(types, type)[0] && !forwardRefAndInjectImport.includes('Inject') && !forwardRefAndInjectImport.includes('forwardRef')) forwardRefAndInjectImport += 'Inject, forwardRef, ';
+  modelsAndServices += computeManyOnlyRelationships(types, type)[1];
+  initRelationships += computeManyOnlyRelationships(types, type)[3];
+  updateRelationships += computeManyOnlyRelationships(types, type)[4];
+
+  relatedRepositoryImport
   return [
     forwardRefAndInjectImport,
     forwardReferencedServices,
@@ -241,26 +247,36 @@ import { ${relationship.type}Service } from './${strings.camelize(relationship.t
   ];
 }
 
-function computeForwardRelationships(type: Type): string[] {
-    const relatedFields = type.fields.filter((field) => field.relation && !field.isEnum && !field.isDeprecated && field.relationType !== 'selfJoinOne' && field.relationType !== 'selfJoinMany');
-    let forwardRelationshipImport = '';
-    let forwardReferencedServices = '';
-    let referencedServices = '';
-    relatedFields.forEach((relatedField) => {
-      if (relatedField.type !== type.typeName) {
-        let relation = type.relationList.find((relation) => relation.type === relatedField.type);
-        if (relation.relatedFieldName) {
-            forwardRelationshipImport = 'forwardRef, ';
-          forwardReferencedServices += `
+function computeForwardRelationships(types: Type[], type: Type): string[] {
+  const relatedFields = type.fields.filter((field) => field.relation && !field.isEnum && !field.isDeprecated && field.relationType !== 'selfJoinOne' && field.relationType !== 'selfJoinMany');
+  let forwardRelationshipImport = '';
+  let forwardReferencedServices = '';
+  let referencedServices = '';
+  relatedFields.forEach((relatedField) => {
+    if (relatedField.type !== type.typeName) {
+      let relation = type.relationList.find((relation) => relation.type === relatedField.type);
+      if (relation.relatedFieldName || computeManyOnlyRelationships(types, type)[0]) {
+        forwardRelationshipImport = 'forwardRef, ';
+        forwardReferencedServices += `
     @Inject(forwardRef(() => ${strings.capitalize(relatedField.type)}Service))
     private readonly ${strings.camelize(relatedField.type)}Service: ${strings.capitalize(relatedField.type)}Service,`;
-        } else {
-          referencedServices += `
+      } else if (relatedField.relationType !== 'manyOnly') {
+        referencedServices += `
     private readonly ${strings.camelize(relatedField.type)}Service: ${strings.capitalize(relatedField.type)}Service,`;
-        }
       }
+    }
+  });
+  forwardReferencedServices += computeManyOnlyRelationships(types, type)[2];
+  const manyOnlyFields = relatedFields.filter(field => field.relationType === 'manyOnly');
+  if (manyOnlyFields.length > 0) {
+    if (!forwardRelationshipImport.includes('forwardRef') && !forwardRelationshipImport.includes('Inject')) forwardRelationshipImport = 'forwardRef, Inject, ';
+    manyOnlyFields.forEach((manyOnlyField) => {
+      forwardReferencedServices += `\n    @Inject(forwardRef(() => ${strings.capitalize(manyOnlyField.type)}Service))
+    private readonly ${strings.camelize(manyOnlyField.type)}Service: ${strings.capitalize(manyOnlyField.type)}Service,`;
     })
-    return [forwardRelationshipImport, forwardReferencedServices, referencedServices];
+  }
+
+  return [forwardRelationshipImport, forwardReferencedServices, referencedServices];
 }
 
 function computePaginationTemplates(types: Type[], type: Type): string[] {
@@ -272,13 +288,13 @@ function computePaginationTemplates(types: Type[], type: Type): string[] {
     paginationArgsImport = 'PaginationArgs, ';
     relatedFields.forEach((relatedField) => {
       const relatedType = types.find((type) => type.typeName === relatedField.type);
-      if (relatedType){
+      if (relatedType) {
         let fieldInRelatedTypeName = relatedType.fields.find((field) => field.type === type.typeName)?.name;
         if (relatedField.relationType === 'selfJoinMany') fieldInRelatedTypeName = `parent${strings.capitalize(pluralize(relatedField.name, 1))}`;
         if (fieldInRelatedTypeName) {
           fieldPaginationImport += `
 import { ${type.typeName}${strings.capitalize(relatedField.name)}Pagination } from './dto/${strings.camelize(type.typeName)}/${strings.camelize(type.typeName)}-${strings.camelize(relatedField.name)}-pagination.dto';`;
-                fieldPaginationMethod += `
+          fieldPaginationMethod += `
   async ${strings.camelize(type.typeName)}${strings.capitalize(relatedField.name)}Pagination(
     ${strings.camelize(type.typeName)}Id: ${type.typeName}['id'],
     args: PaginationArgs,
@@ -304,9 +320,78 @@ import { ${type.typeName}${strings.capitalize(relatedField.name)}Pagination } fr
   }\n`;
         }
       }
-    }); 
+    });
+    const manyOnlyFields = relatedFields.filter(field => field.relationType === 'manyOnly');
+    if (manyOnlyFields.length > 0) {
+      if (!paginationArgsImport.includes('PaginationArgs')) paginationArgsImport = 'PaginationArgs, ';
+      manyOnlyFields.forEach((manyOnlyField) => {
+        fieldPaginationImport += `
+import { ${type.typeName}${strings.capitalize(manyOnlyField.name)}Pagination } from './dto/${strings.camelize(type.typeName)}/${strings.camelize(type.typeName)}-${strings.camelize(manyOnlyField.name)}-pagination.dto';`;
+        fieldPaginationMethod += `
+  async ${strings.camelize(type.typeName)}${strings.capitalize(manyOnlyField.name)}Pagination(
+    ${strings.camelize(type.typeName)}Id: ${type.typeName}['id'],
+    args: PaginationArgs,
+  ): Promise<${type.typeName}${strings.capitalize(manyOnlyField.name)}Pagination> {
+    const [nodes, totalCount] = await this.${strings.camelize(manyOnlyField.type)}Repository.findAndCount({
+      skip: args.skip,
+      take: args.take,
+      where: {
+        ${strings.camelize(type.typeName)}: {
+          id: ${strings.camelize(type.typeName)}Id,
+        },
+      },
+      order: {
+        createdAt:
+          args.sortBy?.createdAt === SortDirection.ASC ? 'ASC' : 'DESC',
+      },
+    });
+
+    return {
+      nodes,
+      totalCount,
+    };
+  }\n`;
+      });
+    }
   }
   return [paginationArgsImport, fieldPaginationImport, fieldPaginationMethod];
+}
+
+function computeManyOnlyRelationships(types: Type[], manyOnlyType: Type): [boolean, string, string, string, string] {
+  //let manyOnlyTemplate = ``;
+  let forwardRefAndInjectImport = false;
+  let importEntitiesAndServicesTemplate = ``;
+  let injectServicesTemplate = ``;
+  let createEntityTemplate = ``;
+  let updateEntityTemplate = ``;
+  //let entitiesToImport: string[] = [];
+  //let entitiesImportTemplate = ``;
+  types.forEach((type) => {
+    const fieldInRelatedType = type.fields.find((field) => field.type === manyOnlyType.typeName)
+    if (fieldInRelatedType && fieldInRelatedType.relationType === 'manyOnly') {
+      forwardRefAndInjectImport = true;
+      // manyOnlyTemplate += `  ${strings.camelize(type.typeName)}Id?: string;\n  ${strings.camelize(type.typeName)}?: I${type.typeName};\n`;
+      // entitiesToImport.push(`${type.typeName}`);
+      importEntitiesAndServicesTemplate += `\nimport { ${type.typeName} } from 'adapters/typeorm/entities/${strings.camelize(type.typeName)}.model';
+import { ${type.typeName}Service } from './${strings.camelize(type.typeName)}.service';`;
+      injectServicesTemplate += `\n    @Inject(forwardRef(() => ${type.typeName}Service))
+    private readonly ${strings.camelize(type.typeName)}Service: ${type.typeName}Service,`;
+      createEntityTemplate += `\n  ${strings.camelize(manyOnlyType.typeName)}.${strings.camelize(type.typeName)} = new ${type.typeName}();
+    if (input.${strings.camelize(type.typeName)}Id) {
+      const ${strings.camelize(type.typeName)} = await this.${strings.camelize(type.typeName)}Service.${strings.camelize(type.typeName)}GetById(input.${strings.camelize(type.typeName)}Id);
+      ${strings.camelize(manyOnlyType.typeName)}.${strings.camelize(type.typeName)}.id = ${strings.camelize(type.typeName)}.id;
+    }`;
+      updateEntityTemplate += `\n  if (input.${strings.camelize(type.typeName)}Id) {
+      ${strings.camelize(manyOnlyType.typeName)}.${strings.camelize(type.typeName)} = new ${type.typeName}();
+      const ${strings.camelize(type.typeName)} = await this.${strings.camelize(type.typeName)}Service.${strings.camelize(type.typeName)}GetById(input.${strings.camelize(type.typeName)}Id);
+      ${strings.camelize(manyOnlyType.typeName)}.${strings.camelize(type.typeName)}.id = ${strings.camelize(type.typeName)}.id;
+    }`;
+    }
+  });
+  //entitiesToImport.filter((item, index) => entitiesToImport.indexOf(item) === index);
+  //entitiesToImport.forEach((entity) => entitiesImportTemplate += `import { I${entity} } from './${entity.toLowerCase()}.interface';\n`);
+
+  return [forwardRefAndInjectImport, importEntitiesAndServicesTemplate, injectServicesTemplate, createEntityTemplate, updateEntityTemplate];
 }
 
 
@@ -314,8 +399,8 @@ import { ${type.typeName}${strings.capitalize(relatedField.name)}Pagination } fr
 function handleSortingInstructions(type: Type): string {
   let sortingInstructions = '';
   type.fields.forEach((field) => {
-    if(field.directives.find((dir: { name: string, args: { name: string, value: string }[] }) => dir.name === 'SortBy'))
-    sortingInstructions += `\n      if (args.sortBy.${field.name} !== null) {
+    if (field.directives.find((dir: { name: string, args: { name: string, value: string }[] }) => dir.name === 'SortBy'))
+      sortingInstructions += `\n      if (args.sortBy.${field.name} !== null) {
         queryBuilder.addOrderBy(
           '${strings.camelize(type.typeName)}.${field.name}',
           args.sortBy.${field.name} === SortDirection.ASC ? 'ASC' : 'DESC',
